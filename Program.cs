ccb188a17d0940d6460ee9c77d1df5e1e4f114ad
barrosjunior26 <barrosjunior26@outlook.com.br>
2026-02-15 19:50:38 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ex022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variáveis globais
+            string[] lista;
+            byte quantidade;
+
+            Console.Clear();
+            Console.WriteLine("========== Lista de compras ==========");
+            Console.Write("Digite a quantidade de itens que deseja adicionar a lista: ");
+            quantidade = Convert.ToByte(Console.ReadLine());
+
+            lista = new string[quantidade];
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                Console.Write($"Informe o {i + 1}º item: ");
+                lista[i] = Convert.ToString(Console.ReadLine());
+            }
+
+            Console.WriteLine("\n\n ========== Itens da sua lista ==========");
+
+            foreach (var itens in lista)
+            {
+                Console.WriteLine(itens);
+            }
+        }
+    }
+}
